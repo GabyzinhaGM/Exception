df2d798a6977082039d0fd36eb8af10c116cbd65
Gabriela Godoy Marques <gabyzinha.ggm@gmail.com>
2022-05-09 16:27:45 -0300

    Calculo usando exception

---
+++ Calculo/src/CalculoMatematico.kt
@@ -0,0 +1,14 @@
+package  calculo
+class CalculoMatematico(primeiroNumero: Int, segundoNumero: Int) {
+    var  primeiroNumero: Int = primeiroNumero
+var  segundoNumero: Int = segundoNumero
+
+fun divisao(): Int{
+    return  (this.primeiroNumero/this.segundoNumero)
+
+
+}
+
+
+
+}
---
+++ Calculo/src/Principal.kt
@@ -0,0 +1,21 @@
+package calculo
+
+fun main() {
+
+   val c: CalculoMatematico = CalculoMatematico(4, 0)
+
+try {
+    println(c.divisao())
+
+
+}
+catch (e: ArithmeticExceptio        n){
+println("Deu ruim!")        e.printStackTrace()
+    
+    
+}
+}
+
+
+
+
